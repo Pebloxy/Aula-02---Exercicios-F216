84a2c35fa87a75d3460ba2e37f0c7e4a3ebb0d13
Pebloxy <pabvnoob2.0@gmail.com>
2026-08-28 08:51:17 -0300

    exercício 5 terminado

--- Exercício 5.c
+++ Exercício 5.c
@@ -5,16 +5,18 @@
 int main() {
     float num1 = 0; 
     float num2 = 0;
+    float result = 0;
  
  printf( "Entre com dois inteiros e eu lhe direi\n" );
- printf( "a divisão " );
+ printf( "a divisão: " );
  
  scanf( "%f%f", &num1, &num2 );
  
  if (num2 == 0){
      printf("Erro: Não é possível dividir por zero!");
  } else {
-     
+     result = (num1 / num2);
+    printf("O resultado é %f\n", result);
  }
     return 0;
 }
